a549318ca3c5fff7db7f804f1d2f52e4ac6a48e5
Samokhin Alexander <icorwin74@gmail.com>
2023-03-15 11:44:17 +0300

    Handle invalid token exceptin

--- gui_chat.py
+++ gui_chat.py
@@ -1,12 +1,15 @@
 import asyncio
 import logging
 import datetime
+import json
+import os
 import aiofiles
 from aiofiles import os as aio_os
 import dotenv
 import configargparse
 import gui
 from socket_manager import open_socket
+from tkinter import messagebox
 
 
 READING_TIMEOUT = 600
@@ -14,6 +17,10 @@ RECONNECT_DELAY = 30
 HISTORY_FILENAME = 'history.txt'
 
 logger = logging.getLogger('minechat')
+
+
+class InvalidToken(Exception):
+    pass
 
 
 def get_datetime_now():
@@ -39,6 +46,53 @@ async def read_msgs(host, port, messages_queue, save_msgs_queue):
             message = f'[{formatted_date}] {message}'
             messages_queue.put_nowait(message)
             save_msgs_queue.put_nowait(message)
+
+
+async def send_msgs(host, port, sending_queue, messages_queue):
+    async with open_socket(host, port) as s:
+        reader, writer = s
+        await handle_chat_reply(reader)
+        chat_token = os.getenv('MINECHAT_TOKEN')
+        nickname = await authorise(reader, writer, chat_token)
+        if not nickname:
+            raise InvalidToken()
+        logger.debug('Log in chat as %s', nickname)
+        print(
+            f'[{get_datetime_now()}] Успешный вход в чат под именем:'
+            f'{nickname}'
+        )
+        while True:
+            message = await sending_queue.get()
+            formatted_date = get_datetime_now()
+            message = f'[{formatted_date}] {nickname}: {message}'
+            messages_queue.put_nowait(message)
+
+
+async def handle_chat_reply(reader, echo=None):
+    future = reader.readline()
+    line = await asyncio.wait_for(future, READING_TIMEOUT)
+    formatted_date = get_datetime_now()
+    decoded_line = line.decode().rstrip()
+    logger.debug('Recieve: %s', decoded_line)
+    if echo:
+        print(f'[{formatted_date}] {decoded_line}')
+    return decoded_line
+
+
+async def authorise(reader, writer, chat_token):
+    writer.write(f'{chat_token}\n'.encode())
+    await writer.drain()
+    authorization_reply = await handle_chat_reply(reader)
+    logger.debug('Authorization reply: %s', authorization_reply)
+    parsed_reply = json.loads(authorization_reply)
+    if parsed_reply is None:
+        print(f'[{get_datetime_now()}] Неизвестный токен: '
+              f'"{chat_token.rstrip()}". '
+              'Проверьте его или зарегистрируйте заново.')
+        logger.error('Token "%s" is not valid', {chat_token.rstrip()})
+        return None
+    await handle_chat_reply(reader)
+    return parsed_reply['nickname']
 
 
 async def load_history(filepath, messages_queue):
@@ -105,6 +159,7 @@ async def main():
         options.port_in,
         options.port_out,
     )
+
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
@@ -116,16 +171,28 @@ async def main():
         history_filename = HISTORY_FILENAME
     await load_history(history_filename, messages_queue)
 
-    await asyncio.gather(
-        gui.draw(messages_queue, sending_queue, status_updates_queue),
-        read_msgs(
-            options.host,
-            options.port_out,
-            messages_queue,
-            save_msgs_queue,
-        ),
-        save_messages(history_filename, save_msgs_queue),
-    )
+    try:
+        await asyncio.gather(
+            gui.draw(messages_queue, sending_queue, status_updates_queue),
+            read_msgs(
+                options.host,
+                options.port_out,
+                messages_queue,
+                save_msgs_queue,
+            ),
+            save_messages(history_filename, save_msgs_queue),
+            send_msgs(
+                options.host,
+                options.port_in,
+                sending_queue,
+                messages_queue,
+            ),
+        )
+    except InvalidToken:
+        messagebox.showerror(
+            'Неверный токен',
+            'Проверьте токен сервер его не узнал!',
+        )
 
 
 if __name__ == '__main__':
